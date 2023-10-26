901ed5ad43c50fdf2ce81019cb4cae982e98233d
Rafael Pavão <93341589+rafaelpavao@users.noreply.github.com>
2023-10-26 10:32:51 -1100

    Update spline_cubico.py

--- spline_cubico.py
+++ spline_cubico.py
@@ -1,8 +1,6 @@
-# Import necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define a function 'spline' to calculate cubic spline coefficients
 def spline(x, y):
     """
     Retorna todos os coeficientes de todos os polinomios
@@ -41,65 +39,66 @@ def spline(x, y):
 
     B.append(0)
 
-    # RESOLVE N SISTEMAs DE QUAÇÕES USANDO NUMPY E SALVA EM DICIONARIO c
+    # RESOLVE N SISTEMAS DE EQUAÇÕES USANDO NUMPY E SALVA EM DICIONARIO  DE COEFICIENTES c
     c = dict(zip(range(n), np.linalg.solve(A, B)))
 
     # INICIALIZANDO DICIONARIO DE COEFICIENTES B E D
     b = {}
     d = {}
 
-    # Calculate 'b' and 'd' coefficients for each segment
+    # CALCULA COEFICIENTES b E d PARA CADA N
     for k in range(n - 1):
         b[k] = (1 / h_xdiff[k]) * (y_dict[k + 1] - y_dict[k]) - (h_xdiff[k] / 3) * (2 * c[k] + c[k + 1])
         d[k] = (c[k + 1] - c[k]) / (3 * h_xdiff[k])
 
-    # Initialize a dictionary 's' to store the spline equations and their domains
+    # INICIALIZA DICIONARIO s O QUAL GUARDA N EQUAÇÕES E SEUS RESPECTIVOS DOMINIOS
     s = {}
     for k in range(n - 1):
-        # Construct the spline equation as a string
+        # CONSTROI EQUAÇÃO COMO UMA STRING
         equacao = f'{y_dict[k]}{b[k]:+}*(x{-x[k]:+}) {c[k]:+}*(x{-x[k]:+})**2 {d[k]:+}*(x{-x[k]:+})**3'
-        s[k] = {'equacao': equacao, 'dominio': [x[k], x[k + 1]]}
+        s[k] = {'Equacao': equacao, 'Dominio': [x[k], x[k + 1]]}
 
     return s
 
-# Define data points (X and Y)
+# DEFINE PONTOS
 X = [2, 25, 40, 90, 110, 140, 165, 180, 198, 212, 215, 220, 223, 225, 226.5, 228.5, 230, 235, 240, 245, 248, 251, 254, 258]
 Y = [50, 65, 70, 75, 75, 74, 72, 70, 69, 78, 78, 78, 78, 78, 78, 62, 60, 58, 56, 52, 54, 52, 54, 50]
 
-# Calculate the spline equations for the given data
+# CALCULA EQUAÇÕES PARA PONTOS ACIMA
 equacoes = spline(X, Y)
 
-# Print the spline equations and their domains
-print("     Segments: ")
+# MOSTRANDO CADA EQUAÇÃO E SEUS DOMINIOS
+print("     Segmentos: ")
 for key, value in equacoes.items():
-    equation_str = value['equacao']
-    domain_str = f"Domain: [{value['dominio'][0]}, {value['dominio'][1]}]"
-    print(f'Segment {key} - {domain_str}\nEquation: {equation_str} \n')
+    equation_str = value['Equacao']
+    domain_str = f"Dominio: [{value['Dominio'][0]}, {value['Dominio'][1]}]"
+    print(f'Segmento {key} - {domain_str}\Equacao: {equation_str} \n')
 
-# Iterate through each spline segment and plot them
+# ITERA DENTRO DE N EQUAÇÕES PARA N SEGMENTOS DE CURVA
 for key, value in equacoes.items():
-    # Define a function 'p(x)' that evaluates the spline equation
+    # DEFINE A FUNÇÃO 'p(x)' A QUAL RESOLVE EQUAÇÃO N
     def p(x):
-        return eval(value['equacao'])
+        return eval(value['Equacao'])
     
-    # Generate a range of x-values within the domain of the spline segment
-    t = np.linspace(*value['dominio'], 100)
+    # SALVA EM t O LIMITE DE X PARA A PLOTAGEM N ( DOMINIO )
+    t = np.linspace(*value['Dominio'], 100)
     
-    # Plot the spline curve
+    # PLOTANDO CURVA PARA EQUAÇÃO N
     plt.plot(t, p(t), label=f'$S_{key}(x)$')
 
+# COLOCANDO IMAGEM NO FUNDO
 im = plt.imread('image.png')
 im = np.flipud(im)
 implot = plt.imshow(im) 
 
-# Scatter plot the original data points
+# INSERINDO PLOTAGEM COMPLETA DOS PONTOS ORIGINAIS
 plt.scatter(X, Y)
 
-# Set axis limits for the plot
+# LIMITANDO AS COORDENADAS DE X E Y PARA MELHOR VIZUALIZAÇÃO DA PLOTAGEM
 plt.axis([0, 270, 0, 190])
 
-# Save the plot as "spline.pdf"
+# SALVA PDF DO GRÁFICO
 plt.savefig("spline.png")
 
-# Display the plot
+# MOSTRAR GRÁFICO
 plt.show()
